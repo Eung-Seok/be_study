02d559a8bf203aed830371807f3c992a6808ffa6
Eung-Seok <rwjflrk1234@gmail.com>
2025-10-30 16:28:29 +0900

    변수 입출력

---
+++ src/be_study/var/variable06.java
@@ -0,0 +1,35 @@
+package be_study.var;
+
+import java.util.Scanner;
+
+public class variable06 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int num1  =scanner.nextInt();
+		
+		int num2 = 10;
+		
+		//값이 같은가? 비교하는 연산자는 ==
+		boolean b1 = (num1 == num2);
+		
+		System.out.println(b1);
+	
+		scanner.nextLine();
+		
+		String str1 = scanner.nextLine();
+		String str2 = "햄버거";
+		
+		System.out.println(str1);
+		System.out.println(str2);
+		System.out.println(str1==str2);
+		System.out.println( str1.equals(str2));
+		
+		//문자열(String) 값 비교할 때에는 ==가 아니라 .equals를 쓰자
+	}
+
+}
